433bf0cc1692d0711856dc186f667221df70954c
amcpoyle <amcpoyle46@gmail.com>
2026-03-07 11:51:53 -0500

    working numerical minimal surface optimizer

--- minimizer.py
+++ minimizer.py
@@ -3,6 +3,8 @@ import sympy as sp
 import casadi as ca
 import matplotlib.pyplot as plt
 
+
+# symbolic
 def compute_gij(f, v_, i, j):
     # v = vars
     u, v = v_
@@ -65,7 +67,7 @@ def compute_nu(f, v_):
     return nu
 
 def compute_gram(f, v_):
-    # g = gram determinant
+    # g = gram determinant = det(gij)
     u, v = v_
     df_du = f.diff(u)
     df_dv = f.diff(v)
@@ -122,22 +124,126 @@ def compute_H_surface(f, v_):
     total = np.trapz(np.trapz(grid, v_range, axis=1), u_range)
     return total
 
+# f = f + H*nu
+# has to vanish on the boundary - ignore?
+# do not iterate over the boundary?
+# take to 0 near boundary manually: f = f + H*nu(u-u0)(u1-u)(v-v0)(v1-v)
+# where U = [u0, u1] x [v0,v1]
+# mean curvature flow implementation
+
+
+# numeric
+def compute_gij(u_val, v_val, i, j):
+    df_ui = None
+    df_uj = None
+    if i == 1:
+        df_ui = f.diff(u)
+    elif i == 2:
+        df_ui = f.diff(v)
+    else:
+        raise ValueError("Error in compute_gij: invalid number 'i'")
+
+    if j == 1:
+        df_uj = f.diff(u)
+    elif j == 2:
+        df_uj = f.diff(v)
+    else:
+        raise ValueError("Error in compute_gij: invalid number 'j'")
+
+    # compute the inner product
+    gij = df_ui.dot(df_uj)
+    return gij
+
+def compute_H_numeric(u_val, v_val):
+    h11 = compute_hij_numeric(f, v_, 1, 1)
+    h12 = compute_hij_numeric(f, v_, 1, 2)
+    h22 = compute_hij_numeric(f, v_, 2, 2)
+
+    g22 = compute_gij_numeric(f, v_, 2, 2)
+    g12 = compute_gij_numeric(f, v_, 1, 2)
+    g11 = compute_gij_numeric(f, v_, 1, 1)
+
+    g = compute_gram_numeric(f, v_)
+
+    H = (1/(2*g))*(h11*g22 - 2*h12*g12 + h22*g11)
+    return H
+
 def minimize_surface():
     pass
 
 def main():
     # define our surface
-    u_range = np.linspace(1e-6, 2*np.pi, 250)
-    v_range = np.linspace(1e-6, 2*np.pi, 250)
-    U, V = np.meshgrid(u_range, v_range)
+    N = 5
+    u_range = np.linspace(1e-6, 2*np.pi, N)
+    v_range = np.linspace(1e-6, 2*np.pi, N)
+
+    du = u_range[1] - u_range[0]
+    dv = v_range[1] - v_range[0]
+    U, V = np.meshgrid(u_range, v_range, indexing='ij')
 
     u, v = sp.symbols('u v')
     f = sp.Matrix([u, v, sp.sin(sp.sqrt(u**2 + v**2))])
+    nu = compute_nu(f, [u,v])
 
-    # plot_surface(f, [u,v], U, V)
+    # calculate f at every point in the mesh
+    f_mesh = np.zeros((N,N, 3))
+    nu_mesh = np.zeros((N,N, 3))
+    phi_mesh = np.zeros((N,N, 3))
+
+    # test = compute_nu(f, [u,v])
+    # print(test.subs({(u, np.pi/4), (v, np.pi/2)}))
+    
+    # initial population of f_mesh
+    for i in range(N):
+        for j in range(N):
+            result = f.subs({(u, u_range[i]), (v, v_range[j])})
+            f_mesh[i][j] = np.array(result).astype(float).flatten()
 
 
-    # test = f.subs([(u, np.pi/4), (v, np.pi/3) ])
+    # initial population of nu_mesh
+    for i in range(N):
+        for j in range(N):
+            result = nu.subs({(u, u_range[i]), (v, v_range[j])})
+            nu_mesh[i][j] = np.array(result).astype(float).flatten()
+
+
+    iter_counter = 0
+    tol_eps = 1e-3
+    current_H_total = 100 # just a starting point
+    current_phi = 1 # init value, f_eps = f
+    eps = 1e-3
+
+    while current_H_total > tol_eps:
+        print("iteration: ", iter_counter)
+
+        integral = 0
+        for i in range(N):
+            for j in range(N):
+                u, v = sp.symbols('u v')
+                perturbation = sp.Matrix((eps * current_phi * nu_mesh[i][j]).tolist())
+                f_eps = f + perturbation
+
+                # compute H
+                H = compute_H(f_eps, [u,v])
+                H_val = H.subs({(u, u_range[i]), (v, v_range[j])}).evalf()
+
+                # compute gram
+                g = compute_gram(f_eps, [u,v])
+                g_val = g.subs({(u, u_range[i]), (v, v_range[j])}).evalf()
+
+                integrand = current_phi*2*float(H_val)*np.sqrt(float(g_val))*du*dv
+                print(integrand)
+                integral += integrand
+        
+        current_H_total = abs(integral)
+        print("Current value: ", current_H_total)
+        current_phi = H_val
+        iter_counter += 1
+
+
+
+
+
 
 
 if __name__ == '__main__':
---
+++ minimizer_numerical.py
@@ -0,0 +1,175 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+def compute_nu(df_du_val, df_dv_val):
+    num = np.cross(df_du_val, df_dv_val)
+    denom = np.linalg.norm(num)
+    nu = num/denom
+    return nu
+
+def compute_gram(df_du_val, df_dv_val):
+    cp = np.cross(df_du_val, df_dv_val)
+    cp_norm = np.linalg.norm(cp)
+    g = cp_norm**2
+    return g
+
+def compute_gij(df_du_val, df_dv_val, i, j):
+    # if i = 1, j = 1 => df_du.dot(df_du)
+    # if i = 1, j = 2 => df_du.dot(df_dv) etc.
+    result = None
+    if i == 1:
+        if j == 1:
+            result = df_du_val.dot(df_du_val)
+        else:
+            result = df_du_val.dot(df_dv_val)
+    else:
+        if j == 1:
+            result = df_dv_val.dot(df_du_val)
+        else:
+            result = df_dv_val.dot(df_dv_val)
+    
+    if result is None:
+        raise ValueError("Error in compute_gij: Invalid number passed")
+
+    return result
+
+def compute_hij(nu_value, df_du2, df_dv2, df_dudv, i, j):
+    result = None
+    if i == 1:
+        if j == 1:
+            result = nu_value.dot(df_du2)
+        else:
+            result = nu_value.dot(df_dudv)
+    else:
+        if j == 1:
+            result = nu_value.dot(df_dudv)
+        else:
+            result = nu_value.dot(df_dv2)
+
+    if result is None:
+        raise ValueError("Error in compute_hij: Invalid number passed")
+
+    return result
+
+def plot_surface(U, V, f_mesh):
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection='3d')
+    ax.plot_surface(U, V, f_mesh[:,:,2], cmap='viridis')
+    plt.show()
+
+
+def main():
+    N = 10
+    u_range = np.linspace(1e-6, 2*np.pi, N)
+    v_range = np.linspace(1e-6, 2*np.pi, N)
+
+    du = u_range[1] - u_range[0]
+    dv = v_range[1] - v_range[0]
+    U, V = np.meshgrid(u_range, v_range, indexing='ij')
+    f_mesh = np.zeros((N,N,3))
+    H_mesh = np.zeros((N,N))
+    nu_mesh = np.zeros((N,N,3))
+
+    for i in range(N):
+        for j in range(N):
+            x_val = u_range[i]
+            y_val = v_range[j]
+            z_val = np.sin(np.sqrt(u_range[i]**2 + v_range[j]**2))
+            f_mesh[i][j] = np.array([x_val, y_val, z_val]).astype(float).flatten()
+
+
+    # plot my original surface
+    plot_surface(U, V, f_mesh)
+
+    tol_eps = 1e-6
+    eps = (min(du, dv)**2)/4
+
+    not_minimal = True
+    counter = 0
+
+    while not_minimal:
+        print("At iter = ", counter)
+
+        # compute df_du and df_dv for our current f_mesh
+        df_du = np.gradient(f_mesh, du, axis=0)
+        df_dv = np.gradient(f_mesh, dv, axis=1)
+        df_du2 = np.gradient(df_du, du, axis=0)
+        df_dv2 = np.gradient(df_dv, dv, axis=1)
+        df_dudv = np.gradient(df_du, dv, axis=1)
+
+        # run our normal variation iteration
+        # compute H at each point on our current surface
+        not_minimal_iter = False
+        H_val_total = 0
+        # NOTE: fixed at the boundary by requirements of minimal surfaces
+        # TODO: is this true
+        for i in range(1, N-1):
+            for j in range(1, N-1):
+                surface_value = f_mesh[i][j]
+                surface_x = surface_value[0]
+                surface_y = surface_value[1]
+                surface_z = surface_value[2]
+
+                df_du_val = df_du[i][j]
+                df_dv_val = df_dv[i][j]
+                df_du2_val = df_du2[i][j]
+                df_dv2_val = df_dv2[i][j]
+                df_dudv_val = df_dudv[i][j]
+
+                # compute nu at this point
+                nu_val = compute_nu(df_du_val, df_dv_val)
+                nu_mesh[i][j] = nu_val
+
+                # compute H at this point
+                h11 = compute_hij(nu_val, df_du2_val, df_dv2_val, df_dudv_val, 1, 1)
+                h22 = compute_hij(nu_val, df_du2_val, df_dv2_val, df_dudv_val, 2,2)
+                h12 = compute_hij(nu_val, df_du2_val, df_dv2_val, df_dudv_val, 1,2)
+                g11 = compute_gij(df_du_val, df_dv_val, 1,1)
+                g22 = compute_gij(df_du_val, df_dv_val, 2,2)
+                g12 = compute_gij(df_du_val, df_dv_val, 1,2)
+                g21 = compute_gij(df_du_val, df_dv_val, 2,1)
+
+                gij = np.array([[g11, g12], [g21,g22]])
+
+                # compute H
+                det_gij = np.linalg.det(gij)
+                prod = h11*g22 - 2*h12*g12 + h22*g11
+                H_val = (1/(2*det_gij))*prod
+                H_mesh[i][j] = H_val
+                H_val_total += H_val**2
+
+
+                if abs(H_val) > tol_eps:
+                    # update my H_mesh because we need to do another iter
+                    not_minimal_iter = True # if we are minimal, will never hit this line
+
+                if not_minimal_iter == False:
+                    not_minimal = False # break out
+                else:
+                    not_minimal = True
+
+        # we are done with sweeping through this mesh
+
+        # if we are still not minimal, then we need to update the mesh
+        if not_minimal:
+            for i in range(N):
+                for j in range(N):
+                    H_ij = H_mesh[i][j]
+                    if abs(H_ij) > tol_eps:
+                        new_val = f_mesh[i][j] + eps*H_mesh[i][j]*nu_mesh[i][j]
+                        f_mesh[i][j] = new_val
+                    else:
+                        continue
+
+
+        counter += 1
+        print(H_val_total)
+
+
+    # plot my new surface
+    plot_surface(U, V, f_mesh)
+
+
+
+if __name__ == '__main__':
+    main()
