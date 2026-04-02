major fixes to derivative calculations and neck pinch singularity code

minimizer_examples.py
@@ -1,6 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
-from minimizer_numerical import plot_surface, run_minimizer, run_minimizer_animation
+from minimizer_numerical import plot_surface, run_minimizer, run_minimizer_animation, run_minimizer_blender
 import plotly.graph_objects as go
 from PIL import Image
 import io
@@ -27,12 +27,16 @@ def surface1(dur):
 
 
     original_fig = plot_surface(U, V, f_mesh)
-    tol_eps = 1e-6
+    tol_eps = 1e-3
     eps = (min(du, dv)**2)/4
     f_mesh_min, H_mesh_min, nu_mesh_min, plotly_frames = run_minimizer_animation(N, u_range, v_range, du, dv,
                                                         U, V, f_mesh, H_mesh, nu_mesh,
-                                                        tol_eps, eps, 25)
+                                                        tol_eps, eps, 15, fix_u_boundary=True, fix_v_boundary=True, periodic=False)
 
+    # blender_frames = np.array(blender_frames)
+    # np.save("surface1_frames.npy", blender_frames)
+
+    # CODE FOR PLOTLY ANIMATION:
     fig = go.Figure(
             data = go.Surface(
                 x=f_mesh[:,:,0],
@@ -125,6 +129,100 @@ def right_helicoid():
     original_fig.show()
     new_fig.show()
     plt.show()
+
+
+
+def sphere(dur):
+    N = 25
+    z_range = np.linspace(-1, 1, N) # z
+    theta_range = np.linspace(0, 2*np.pi, N) # theta
+
+    dz = z_range[1] - z_range[0]
+    dtheta = theta_range[1] - theta_range[0]
+    T, Z = np.meshgrid(theta_range, z_range, indexing='ij')
+    f_mesh = np.zeros((N,N,3))
+    H_mesh = np.zeros((N,N))
+    nu_mesh = np.zeros((N,N,3))
+
+    r = 1
+
+    for i in range(N):
+        for j in range(N):
+            theta_val = theta_range[i]
+            z_val = z_range[j]
+            x_val = np.sqrt(1 - z_val**2)*np.cos(theta_val)
+            y_val = np.sqrt(1 - z_val**2)*np.sin(theta_val)
+            z_val = z_val
+            f_mesh[i][j] = np.array([x_val, y_val, z_val]).astype(float).flatten()
+
+
+
+    # plot my original surface
+    original_fig = plot_surface(T, Z, f_mesh)
+    # original_fig.show()
+    # plt.show()
+
+    tol_eps = 1e-6
+    eps = (min(dtheta, dz)**2)/4
+
+    f_mesh_min, H_mesh_min, nu_mesh_min, plotly_frames = run_minimizer_animation(N, theta_range, z_range, dtheta, dz,
+                                                        T, Z, f_mesh, H_mesh, nu_mesh,
+                                                        tol_eps, eps, 15, fix_u_boundary=False, fix_v_boundary=False, periodic=True)
+    
+
+    fig = go.Figure(
+            data = go.Surface(
+                x=f_mesh[:,:,0],
+                y=f_mesh[:,:,1],
+                z=f_mesh[:,:,2],
+                colorscale='Viridis',
+                cmin=-1,cmax=1
+            ),
+            frames = plotly_frames
+        )
+
+    fig.update_layout(
+            updatemenus=[
+                dict(
+                    type='buttons',
+                    showactive=False,
+                    y=0,
+                    x=0.5,
+                    xanchor='center',
+                    buttons=[
+                        dict(
+                            label='Play',
+                            method='animate',
+                            args=[None, dict(frame=dict(duration=dur, redraw=True), fromcurrent=True)]
+                            )
+                        ]
+                    )
+                ]
+            )
+    
+    fig.update_layout(
+            sliders=[
+                dict(
+                    steps=[
+                        dict(
+                            method='animate',
+                            args=[[frame.name], dict(mode='immediate', frame=dict(duration=dur, redraw=True))],
+                            label=str(i*10)
+                            )
+                        for i, frame in enumerate(plotly_frames)
+                        ],
+                    currentvalue=dict(prefix='Iteration: '),
+                    x=0.1,
+                    len=0.9
+                    )
+                ]
+            )
+
+    fig.show()
+    # new_fig = plot_surface(U, V, f_mesh)
+    # original_fig.show()
+    # new_fig.show()
+    # plt.show()
 
    
 def main():

minimizer_numerical.py
@@ -74,6 +74,7 @@ def run_minimizer(N, u_range, v_range, du, dv, U, V, f_mesh, H_mesh, nu_mesh, to
         print("At iter = ", counter)
 
         # compute df_du and df_dv for our current f_mesh
+        # TODO: looks if that's the issue
         df_du = np.gradient(f_mesh, du, axis=0)
         df_dv = np.gradient(f_mesh, dv, axis=1)
         df_du2 = np.gradient(df_du, du, axis=0)
@@ -152,7 +153,7 @@ def run_minimizer(N, u_range, v_range, du, dv, U, V, f_mesh, H_mesh, nu_mesh, to
         
     return f_mesh, H_mesh, nu_mesh
 
-def run_minimizer_animation(N, u_range, v_range, du, dv, U, V, f_mesh, H_mesh, nu_mesh, tol_eps, eps, frame_freq):
+def run_minimizer_animation(N, u_range, v_range, du, dv, U, V, f_mesh, H_mesh, nu_mesh, tol_eps, eps, frame_freq, fix_u_boundary=True, fix_v_boundary=True, periodic=False):
 
     not_minimal = True
     counter = 0
@@ -163,16 +164,127 @@ def run_minimizer_animation(N, u_range, v_range, du, dv, U, V, f_mesh, H_mesh, n
 
         # add our mesh to plotly_frames
         if counter % frame_freq == 0:
+            f_plot = np.concatenate([f_mesh, f_mesh[0:1]], axis=0) if periodic else f_mesh
             frame = go.Frame(
                     data = go.Surface(
-                        x=f_mesh[:,:,0],
-                        y=f_mesh[:,:,1],
-                        z=f_mesh[:,:,2],
+                        x=f_plot[:,:,0],
+                        y=f_plot[:,:,1],
+                        z=f_plot[:,:,2],
                         colorscale='Viridis',
                         cmin=-1,cmax=1),
                     name = str(counter)
                 )
             plotly_frames.append(frame)
+
+        df_dv = np.gradient(f_mesh, dv, axis=1)
+        df_dv2 = np.gradient(df_dv, dv, axis=1)
+
+        if periodic:
+            df_du = (np.roll(f_mesh, -1, axis=0) - np.roll(f_mesh, 1, axis=0)) / (2*du)
+            df_du2 = (np.roll(f_mesh, -1, axis=0) - 2*f_mesh + np.roll(f_mesh, 1, axis=0)) / (du**2)
+            df_dudv = (np.roll(df_dv, -1, axis=0) - np.roll(df_dv, 1, axis=0)) / (2*du)
+        else:
+            df_du = np.gradient(f_mesh, du, axis=0)
+            df_du2 = np.gradient(df_du, du, axis=0)
+            df_dudv = np.gradient(df_du, dv, axis=1)
+
+
+        # run our normal variation iteration
+        # compute H at each point on our current surface
+        not_minimal_iter = False
+        H_val_total = 0
+        # NOTE: fixed at the boundary by requirements of minimal surfaces
+        # TODO: is this true
+        # TODO: neck pinch singularity don't fix?
+        i_range = range(N) if (periodic or not fix_u_boundary) else range(1, N-1)
+        j_range = range(1, N-1) if fix_v_boundary else range(N)
+        for i in i_range:
+            for j in j_range:
+                surface_value = f_mesh[i][j]
+                surface_x = surface_value[0]
+                surface_y = surface_value[1]
+                surface_z = surface_value[2]
+
+                df_du_val = df_du[i][j]
+                df_dv_val = df_dv[i][j]
+                df_du2_val = df_du2[i][j]
+                df_dv2_val = df_dv2[i][j]
+                df_dudv_val = df_dudv[i][j]
+
+                # compute nu at this point
+                nu_val = compute_nu(df_du_val, df_dv_val)
+                nu_mesh[i][j] = nu_val
+
+                # compute H at this point
+                h11 = compute_hij(nu_val, df_du2_val, df_dv2_val, df_dudv_val, 1, 1)
+                h22 = compute_hij(nu_val, df_du2_val, df_dv2_val, df_dudv_val, 2,2)
+                h12 = compute_hij(nu_val, df_du2_val, df_dv2_val, df_dudv_val, 1,2)
+                g11 = compute_gij(df_du_val, df_dv_val, 1,1)
+                g22 = compute_gij(df_du_val, df_dv_val, 2,2)
+                g12 = compute_gij(df_du_val, df_dv_val, 1,2)
+                g21 = compute_gij(df_du_val, df_dv_val, 2,1)
+
+                gij = np.array([[g11, g12], [g21,g22]])
+
+                # compute H
+                det_gij = np.linalg.det(gij)
+                prod = h11*g22 - 2*h12*g12 + h22*g11
+                H_val = (1/(2*det_gij))*prod
+                H_mesh[i][j] = H_val
+                H_val_total += H_val**2
+
+                if np.isnan(H_val):
+                    not_minimal = False # break out
+
+
+                if abs(H_val) > tol_eps:
+                    # update my H_mesh because we need to do another iter
+                    not_minimal_iter = True # if we are minimal, will never hit this line
+
+                if not_minimal_iter == False:
+                    not_minimal = False # break out
+                else:
+                    not_minimal = True
+
+        # we are done with sweeping through this mesh
+
+        # if we are still not minimal, then we need to update the mesh
+        if not_minimal:
+            for i in range(N):
+                for j in range(N):
+                    H_ij = H_mesh[i][j]
+                    if abs(H_ij) > tol_eps:
+                        new_val = f_mesh[i][j] + eps*H_mesh[i][j]*nu_mesh[i][j]
+                        f_mesh[i][j] = new_val
+                    else:
+                        continue
+
+
+        # seam consistency...
+        # f_mesh[N-1] = f_mesh[0]
+
+        counter += 1
+        print('TOTAL = ', H_val_total)
+
+        if (np.isnan(H_val_total) | np.isinf(H_val_total)):
+            not_minimal = False # break out
+
+
+    return f_mesh, H_mesh, nu_mesh, plotly_frames
+
+
+def run_minimizer_blender(N, u_range, v_range, du, dv, U, V, f_mesh, H_mesh, nu_mesh, tol_eps, eps, frame_freq):
+
+    not_minimal = True
+    counter = 0
+    blender_frames = []
+
+    while not_minimal:
+        print("At iter = ", counter)
+
+        # add our mesh to blender_frames
+        if counter % frame_freq == 0:
+            blender_frames.append(f_mesh.copy())
 
         # compute df_du and df_dv for our current f_mesh
         df_du = np.gradient(f_mesh, du, axis=0)
@@ -257,4 +369,4 @@ def run_minimizer_animation(N, u_range, v_range, du, dv, U, V, f_mesh, H_mesh, n
             not_minimal = False # break out
 
 
-    return f_mesh, H_mesh, nu_mesh, plotly_frames
+    return f_mesh, H_mesh, nu_mesh, blender_frames

neck_singularity.py
@@ -1,6 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
-from minimizer_numerical import plot_surface, run_minimizer, run_minimizer_animation
+from minimizer_numerical import plot_surface, run_minimizer, run_minimizer_animation, run_minimizer_blender
 import plotly.graph_objects as go
 from PIL import Image
 import io
@@ -18,7 +18,7 @@ def r(z):
 
 def quartic_eqn(dur):
     N = 25
-    theta_range = np.linspace(0, 2*np.pi, N)
+    theta_range = np.linspace(0, 2*np.pi, N, endpoint=False)
     z_range = np.linspace(-1,1, N)
 
     dtheta = theta_range[1] - theta_range[0]
@@ -46,13 +46,19 @@ def quartic_eqn(dur):
     eps = (min(dtheta, dz)**2)/4
     f_mesh_min, H_mesh_min, nu_mesh_min, plotly_frames = run_minimizer_animation(N, theta_range, z_range, dtheta, dz,
                                                         T, Z, f_mesh, H_mesh, nu_mesh,
-                                                        tol_eps, eps, 10)
+                                                        tol_eps, eps, 5, fix_u_boundary=False, fix_v_boundary=True, periodic=True) # fix z boundary but not theta boundary
+
+    # blender_frames = np.array(blender_frames)
+    # np.save("neck_pinch_frames3.npy", blender_frames)
+
+    # fig_plot = 
+    f_plot = np.concatenate([f_mesh, f_mesh[0:1]], axis=0)
 
     fig = go.Figure(
             data = go.Surface(
-                x=f_mesh[:,:,0],
-                y=f_mesh[:,:,1],
-                z=f_mesh[:,:,2],
+                x=f_plot[:,:,0],
+                y=f_plot[:,:,1],
+                z=f_plot[:,:,2],
                 colorscale='Viridis',
                 cmin=-1,cmax=1
             ),